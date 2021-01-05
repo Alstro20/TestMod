f826e63f8363f72d86372be4b44b5627456c54e1
Alstro20 <sb74851@gmail.com>
2021-01-04 23:40:37 -0600

    yeah

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -15,6 +15,8 @@ public class ExampleMod
     public static final String VERSION = "1.0";
 
     private static Logger logger;
+    
+    //Test Change 1/4/21
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
