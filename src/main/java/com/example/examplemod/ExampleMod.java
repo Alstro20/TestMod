b7e87418aceaa7414a90218b42c923899054a799
Alstro20 <sb74851@gmail.com>
2021-01-07 00:08:45 -0600

    yeah

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,6 +1,5 @@
 package com.example.examplemod;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
